6cfa376880386075ff2b1612b918849e7c80796f
TheBakedPotato <11617336+TheBakedPotato@users.noreply.github.com>
2025-11-16 13:22:43 -0500

    feature/#3: Added initial data dir with text file

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <LittleFS.h>
 
 #include <Startup.h>
 
@@ -8,6 +9,26 @@ void setup()
 
   splash::Startup splash;
   splash.print(Serial);
+
+  LittleFS.begin(true);
+
+  bool fileExists = LittleFS.exists("/test.txt");
+
+  if (fileExists)
+  {
+    Serial.println("'test.txt' exists!");
+    File testFile = LittleFS.open("/test.txt");
+
+    uint8_t buffer[100];
+    testFile.read(buffer, 100);
+
+    char *characterBuffer = (char *)(buffer);
+    Serial.print(characterBuffer);
+  }
+  else
+  {
+    Serial.println("'test.txt' DOES NOT exists! >:(");
+  }
 }
 
 void loop()
